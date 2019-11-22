485f449ea62d1df84f1ebaa241ef6b03d04fae67
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2019-11-22 15:38:28 -0500

    initial commit of france2018 (static)
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>
    Signed-off-by: Eric Poirier <eric.poirier@eclipse-foundation.org>

---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+/*!
+ * Copyright (c) 2018 Eclipse Foundation, Inc.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * Contributors:
+ *   Christopher Guindon <chris.guindon@eclipse-foundation.org>
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+*/
+
+mix = require('laravel-mix');
+mix.options({uglify: {uglifyOptions: {compress: false, output: {comments: true}}}});
+
+//mix.setPublicPath('static');
+//mix.setResourceRoot('../');
+
+mix.less('./less/france2018/styles.less', './content/france2018/styles.css');
